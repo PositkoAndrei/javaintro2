f81b315c80fb444fbeab5bbd71f2494ba95d8a37
Andrei <positko.andrei@gmail.com>
2019-02-08 00:08:04 +0200

    Month If else

---
+++ src/Month_if.java
@@ -0,0 +1,46 @@
+public class Month_if {
+    public static void main (String[] args){
+        final int NumberOfMonth=12;
+            System.out.println("IF ПРИМЕР:");
+            if (NumberOfMonth == 1) {
+            System.out.println("Январь");
+            if (NumberOfMonth == 2) {
+                System.out.println("Февраль");
+            }else if (NumberOfMonth == 3) {
+                System.out.println("Март");
+            }else if (NumberOfMonth == 4) {
+                System.out.println("Апрель");
+            }else if (NumberOfMonth == 5) {
+                System.out.println("Май");
+            }else if (NumberOfMonth == 6) {
+                System.out.println("Июнь");
+            }else if (NumberOfMonth == 7) {
+                System.out.println("Июль");
+            }else if (NumberOfMonth == 8) {
+                System.out.println("Август");
+            }else if (NumberOfMonth == 9) {
+                System.out.println("Сентябрь");
+            }else if (NumberOfMonth == 10) {
+                System.out.println("Октябрь");
+            }else if (NumberOfMonth == 11) {
+                System.out.println("Ноябрь");
+            }else if (NumberOfMonth == 12) {
+                System.out.println("Декабрь");
+            }else if (NumberOfMonth <=1) {
+                System.out.println("Ошибка");
+            }else if (NumberOfMonth >=12) {
+                System.out.println("Ошибка");
+
+
+            }
+
+
+
+            }
+        }
+
+        }
+
+
+
+
