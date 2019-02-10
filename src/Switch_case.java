6c83d20cf5cdd168b55d4387bbd695f5b6efea7f
Andrei <positko.andrei@gmail.com>
2019-02-10 17:33:55 +0200

    Month Switch

--- src/Switch_case.java
+++ src/Switch_case.java
@@ -1,29 +1,40 @@
 public class Switch_case {
     public static void main (String [] args){
-        final int NumberOfMonth=12;
-        switch(NumberOfMonth){
+        final int NumberOfMonth=3;
+                switch(NumberOfMonth){
             case 1:
                 System.out.println("Январь");
+                break;
             case 2:
                 System.out.println("Февраль");
+                break;
             case 3:
                 System.out.println("Март");
+                break;
             case 4:
                 System.out.println("Апрель");
+                break;
             case 5:
                 System.out.println("Май");
+                break;
             case 6:
                 System.out.println("Июнь");
+                break;
             case 7:
                 System.out.println("Июль");
+                break;
             case 8:
                 System.out.println("Август");
+                break;
             case 9:
                 System.out.println("Сентябрь");
+                break;
             case 10:
                 System.out.println("Октябрь");
+                break;
             case 11:
                 System.out.println("Ноябрь");
+                break;
             case 12:
                 System.out.println("Декабрь");
                 break;
