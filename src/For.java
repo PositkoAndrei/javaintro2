1dafe62bc888aa45683ec3e404932e95fd07c8eb
Andrei <positko.andrei@gmail.com>
2019-02-10 22:15:43 +0200

    Home Work

---
+++ src/For.java
@@ -0,0 +1,11 @@
+public class For {
+    public static void main (String [] args){
+        int count=1;
+        int a=7;
+        for (count=1; count<=15; count++){
+            System.out.println("Номер числа:" + count + ", число " + a+ " ");
+            a+=7;
+        }
+    }
+}
+
